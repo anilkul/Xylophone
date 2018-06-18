6ae323d7a2fa8f24630893e6643848a6a954e7ab
anilkul <mehmetanilkul@gmail.com>
2018-06-18 20:02:03 +0300

    First Commit

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var noteSnd: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
 
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
-        
+        let note = Int(sender.tag)
+        playNote(note: note)
     }
     
-  
-
+    func playNote(note: Int) {
+        let path = Bundle.main.path(forResource: "note\(note)", ofType: "wav")
+        print("note\(note)")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            //tanimladigimiz URL kullanilarak audio stream yapilacak
+            try noteSnd = AVAudioPlayer(contentsOf: soundURL)
+            //calmaya hazirla
+            noteSnd.prepareToPlay()
+            } catch let err as NSError {
+                //stream yapilamazsa error ver
+                print(err.debugDescription)
+            }
+        
+        if noteSnd.isPlaying {
+                noteSnd.stop()
+        }
+        
+        noteSnd.play()
+    }
 }
 
